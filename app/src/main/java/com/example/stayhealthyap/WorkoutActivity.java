f901c73f9d3823a306e13588c727057fce1680bb
alexanne <alexannefortin18@gmail.com>
2025-11-16 21:03:20 -0500

    ui for workouts page, not the fianl design

---
+++ app/src/main/java/com/example/stayhealthyap/WorkoutActivity.java
@@ -0,0 +1,30 @@
+package com.example.stayhealthyap;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_workout);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+    //btns need to chnag when they click pause to jus be buttond saying resume or end
+    //need to make the nav work
+    //timer
+    // speed, and steps gonna be auto calc easier ig and at the end we use speed and duration and maybe steps to calc the cals burnt
+}
